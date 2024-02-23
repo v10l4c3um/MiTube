7a68052a176f13d66d194b8fb234dce3076a010f
4nThonyIO <gamalianton@gmali.com>
2024-02-23 19:09:36 +0100

    Coommnet model added

--- MiTubeAPI/Models/Commnet.cs
+++ MiTubeAPI/Models/Commnet.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MiTubeModels
 {
-    public class Comment
+    public class Comment : Model
     {
+        [Required]
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; }
+
+        [Required]
+        public Guid VideoId { get; set; }
+        public virtual Video Video { get; set; }
+
+
+        public string? Value { get; set; }
+        public Guid? ParentId { get; set; }
+
+        [Timestamp]
+        public DateTime Timestamp { get; set; }
+
+        public Guid? ReferenceUserId { get; set; }
 
     }
 }
